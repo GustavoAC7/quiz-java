dcee29f1f601d0162fb106e13ac81d895580f9e5
GustavoAC7 <163939909+GustavoAC7@users.noreply.github.com>
2024-05-23 19:15:29 -0300

    cabecalho.java

---
+++ cabecalho.java
@@ -0,0 +1,15 @@
+public class cabecalho {
+    public static String Faculdade = "";
+    public static String Aluno = "";
+    public static String Professor = "";
+    public static String Disciplina = "";
+    public static String Inicio = "";
+    public void escrevaCabecalho(){
+
+        System.out.println(this.Faculdade);
+        System.out.println(this.Aluno);
+        System.out.println(this.Professor);
+        System.out.println(this.Disciplina);
+        System.out.println(this.Inicio);
+    }
+}
